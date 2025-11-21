93ec1eb5c0d32a5558fd19f1df7361b5fe04f54f
Lakeshsaikumar <keerthilakesh77@gmail.com>
2025-11-21 12:13:16 +0530

    Update HibernateConfiguration.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/HibernateConfiguration.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/HibernateConfiguration.java
@@ -1,72 +1,50 @@
 package com.jtspringproject.JtSpringProject;
- 
+
 import java.util.Properties;
- 
+
 import javax.sql.DataSource;
- 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
- 
+
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfiguration {
-    @Value("${db.driver}")
-    private String DRIVER;
- 
-    @Value("${db.password}")
-    private String PASSWORD;
- 
-    @Value("${db.url}")
-    private String URL;
- 
-    @Value("${db.username}")
-    private String USERNAME;
- 
-    @Value("${hibernate.dialect}")
-    private String DIALECT;
- 
-    @Value("${hibernate.show_sql}")
-    private String SHOW_SQL;
- 
-    @Value("${hibernate.hbm2ddl.auto}")
-    private String HBM2DDL_AUTO;
- 
-    @Value("${entitymanager.packagesToScan}")
-    private String PACKAGES_TO_SCAN;
- 
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(DRIVER);
-        dataSource.setUrl(URL);
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
+        // Spring Boot properties will automatically configure these if present in application.properties
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); // MySQL Driver
+        dataSource.setUrl("jdbc:mysql://localhost:3306/jtprojectdb1?useSSL=false&serverTimezone=UTC");
+        dataSource.setUsername("lakesh");
+        dataSource.setPassword("lakesh@dwp");
         return dataSource;
     }
- 
+
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
+        sessionFactory.setPackagesToScan("com.jtspringproject.JtSpringProject.model"); // replace with your entity package
+
         Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", DIALECT);
-        hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
-        hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+
         sessionFactory.setHibernateProperties(hibernateProperties);
- 
         return sessionFactory;
     }
- 
+
     @Bean
     public HibernateTransactionManager transactionManager() {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
         transactionManager.setSessionFactory(sessionFactory().getObject());
         return transactionManager;
-    }   
+    }
 }
